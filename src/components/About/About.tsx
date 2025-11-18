004c0a1c0e0837aa1238c2467e2cd1a6cd9caa6f
SmokyDaBear <jeshurungreen@gmail.com>
2025-11-17 22:11:58 -0600

    Updates

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,23 +1,22 @@
 import "./about.css";
 import kitchenImg from "../../assets/location/food-prep-area.jpeg";
+import kitchenImg2 from "../../assets/sifting.jpg";
+import kneadingImg from "../../assets/kneading.jpg";
 import remmy from "../../assets/remy.jpg";
 
 export default function About() {
   return (
     <section className="about-section">
-      <img src={kitchenImg} alt="Bakery kitchen with baked goods on display" />
+      <img
+        src={kitchenImg}
+        width={500}
+        alt="Bakery kitchen with baked goods on display"
+      />
       <h2>About Our Bakery</h2>
       <p>
         Welcome to our cozy bakery, where we specialize in crafting delicious
         desserts and brewing the perfect cup of coffee. Our passion for baking
         and coffee-making is evident in every bite and sip you take.
-      </p>
-      <h3>Our Founder</h3>
-      <p>
-        Founded by Remmy in 2010, we have been serving the community with our
-        artisanal baked goods and premium coffee for over a decade. Remmy is a
-        trained pastry chef and coffee enthusiast who believes in the power of
-        good food to bring people together.
       </p>
       <div className="profile-card">
         <img src={remmy}></img>
@@ -26,7 +25,21 @@ export default function About() {
           <p>Founder & Head Baker</p>
         </div>
       </div>
+      <h3>Our Founder</h3>
+
+      <p>
+        Founded by Remmy in 2010, we have been serving the community with our
+        artisanal baked goods and premium coffee for over a decade. Remmy is a
+        trained pastry chef and coffee enthusiast who believes in the power of
+        good food to bring people together.
+      </p>
+
       <h3>Our Commitment to Quality</h3>
+      <img
+        src={kitchenImg2}
+        width={600}
+        alt="Sifting flour in bakery kitchen"
+      />
       <p>
         We use only the finest ingredients, sourcing locally whenever possible,
         to ensure that every item on our menu is fresh and flavorful. From flaky
@@ -39,6 +52,8 @@ export default function About() {
         best flavors in every cup. Whether you prefer a classic espresso or a
         creamy latte, our baristas are dedicated to crafting your perfect brew.
       </p>
+      <img src={kneadingImg} width={600} alt="Kneading dough in bakery" />
+      <h3>Community Focus</h3>
       <p>
         We believe that a great bakery is more than just a place to grab a quick
         treat; it's a community hub where friends and family can gather to enjoy
